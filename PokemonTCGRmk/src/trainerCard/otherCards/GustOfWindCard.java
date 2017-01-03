1097bb9962c245318518c14623783fe7fc1c95f6
ianparr95 <ianparr95@gmail.com>
2017-01-03 11:53:47 -0800

    aaa

---
+++ PokemonTCGRmk/src/trainerCard/otherCards/GustOfWindCard.java
@@ -0,0 +1,105 @@
+package trainerCard.otherCards;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import arena.Arena;
+import cardAbstract.ActivePokemonCard;
+import cardAbstract.Card;
+import cardAbstract.CardRequest;
+import cardAbstract.TrainerCard;
+import trainerCard.statusCards.Potion;
+
+public class GustOfWindCard extends TrainerCard {
+
+	public GustOfWindCard(Arena arena, int id) {
+		super(arena, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean attachable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlay() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void whenPlayed() throws CardRequest {
+		// TODO Auto-generated method stub
+		List<Card> pList = new ArrayList<Card>();
+		for (Card c : arena.getPlayerDef().getBench().getBench()) {
+			pList.add(c);
+		}
+		CardRequest pc = new CardRequest(true, 1, pList, pList
+				, "Choose a benched pokemon to switch with the active.", 0, false);
+		pc.requestCard();
+	}
+	
+	@Override
+	public void returnRequestedCards(CardRequest pc) {
+		// Swap out with it:
+		if (pc.getReturnList().size() == 0) {
+			System.err.println("Did not specify pokemon to switch.");
+			return;
+		}
+		ActivePokemonCard apc = (ActivePokemonCard) pc.getReturnList().get(0);
+		if (apc == null) {
+			System.err.println("Did not specify pokemon to receive potion.");
+			return;
+		} else {
+			// Good: can swap
+			ActivePokemonCard curAct = arena.getDef();
+			arena.setDefPokemon(apc);
+			arena.getPlayerDef().getBench().removeCard(apc);
+			int chp = curAct.getDamage();
+			arena.getPlayerDef().getBench().add(curAct);
+			for (ActivePokemonCard aa : arena.getPlayerDef().getBench().getBench()) {
+				if (aa.equals(curAct)) {
+					aa.setDamage(chp);
+					aa.getStatus().clear();
+					System.out.println("Set damage");
+					break;
+				}
+			}
+			arena.getPlayerAtt().getHand().removeCardToPile(this);
+			return;
+		}
+	}
+
+	@Override
+	public void turnOppToUs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnUsToOpp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Card copyCard() {
+		// TODO Auto-generated method stub
+		return new GustOfWindCard(arena, getId());
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Gust of Wind";
+	}
+
+	@Override
+	public String addInfoName() {
+		// TODO Auto-generated method stub
+		return "Choose 1 of your opponent's Benched Pokémon and switch it with his or her Active Pokémon.";
+	}
+
+}
